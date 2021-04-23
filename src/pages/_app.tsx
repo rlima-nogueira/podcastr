0b5da6315094eeced7acc44447a02dd7e2040385
Rafaela Lima Nogueira <rafaela.nogueira@meta.com.br>
2021-04-23 11:04:39 -0300

    useRef, context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,15 +10,24 @@ function MyApp({ Component, pageProps }) {
 
   const [episodeList, setEpisodeList] = useState([]);
   const [currentEpisodeIndex, setCurrentEpisodeList] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   function play(episode) {
     setEpisodeList([episode]);
     setCurrentEpisodeList(0);
+    setIsPlaying(true);
   }
 
+  function togglePlay() {
+    setIsPlaying(!isPlaying);
+  }
+
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state);
+  }
 
   return (
-    <PlayerContext.Provider value={{episodeList, currentEpisodeIndex, play}}>
+    <PlayerContext.Provider value={{episodeList, currentEpisodeIndex, play, isPlaying, togglePlay, setPlayingState}}>
       <div className={styles.wrapper}>
         <main>
           <Header/>
